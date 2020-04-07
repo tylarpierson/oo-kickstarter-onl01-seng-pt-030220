b7e48b736286888cb66b1c8b812fa751f2612157
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-07 16:49:29 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,7 @@
 class Backer 
-  attr_reader :backed_projects
-  def initialize(backed_projects)
+  attr_reader :backed_projects, :name 
+  def initialize(name)
+    @name = name 
     @backed_projects = []
   end 
 end 
